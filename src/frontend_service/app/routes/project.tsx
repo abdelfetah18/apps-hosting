5d12499ce5516bc4ae7a6ff17a9d13eac7e585cf
abdelfetah18 <nstart0319@gmail.com>
2026-01-16 15:39:01 +0100

    Implement basic apps listing search

--- src/frontend_service/app/routes/project.tsx
+++ src/frontend_service/app/routes/project.tsx
@@ -5,6 +5,8 @@ import Iconify from "~/components/Iconify";
 import { RUNTIMES_LOGOS } from "~/consts";
 import { getProjectById } from "~/services/project_service";
 import { listApps } from "~/services/app_service";
+import { useState } from "react";
+import { textContainsQuery } from "~/helpers/utils";
 
 export async function clientLoader({ params }: Route.LoaderArgs) {
     const projectId = params.project_id;
@@ -25,8 +27,10 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
 }
 
 export default function Project({ params, loaderData }: Route.ComponentProps) {
+    const [query, setQuery] = useState("");
+
     const project = loaderData.project;
-    const apps = loaderData.apps;
+    const apps = loaderData.apps.filter(app => textContainsQuery(app.name, query));
 
     return (
         <div className="w-full flex flex-col px-16 py-8 overflow-auto">
@@ -38,7 +42,13 @@ export default function Project({ params, loaderData }: Route.ComponentProps) {
                                 <Link to={`/projects/${params.project_id}/apps/create`} className="w-fit bg-purple-700 px-8 py-1 rounded-lg text-sm text-white font-medium">New App</Link>
                                 <div className="flex items-center gap-2 text-sm border border-gray-400 rounded-lg px-4 py-1">
                                     <Iconify icon="tabler:search" />
-                                    <input type="text" placeholder="Search for an app" className="outline-none border-none" />
+                                    <input
+                                        type="text"
+                                        placeholder="Search for an app"
+                                        className="outline-none border-none"
+                                        value={query}
+                                        onChange={(ev) => setQuery(ev.target.value)}
+                                    />
                                 </div>
                             </div>
                             <div className="w-full flex flex-col gap-2">
